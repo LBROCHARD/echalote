5a59ac3fb46f2c11a29877c960a69143a998e055
LBROCHARD <lewis.brochard@gmail.com>
2025-07-10 11:45:03 +0200

    test: testing lighhouse ci

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,13 +1,12 @@
 module.exports = {
     ci: {
         collect: {
-            staticDistDir: "./dist/assets/index-CgxRXJpN.js"
-            // "startServerCommand": "npm run start",
-            // "startServerReadyPattern": "Local:",
-            // "url": [
-            //     "http://localhost:3000/",
-            // ],
-            // "numberOfRuns": 1
+            startServerCommand: "npm run start",
+            startServerReadyPattern: "Local:",
+            url: [
+                "http://localhost:3000/",
+            ],
+            numberOfRuns: 1
         },
         assert: {
 			"assertions": {
