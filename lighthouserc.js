7c5763f22f1d076713d99fe95105e4613a18219d
LBROCHARD <lewis.brochard@gmail.com>
2025-07-10 11:21:07 +0200

    feat: created CI github actions

---
+++ lighthouserc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    ci: {
+        collect: {
+            staticDistDir: "./dist/assets/index-CgxRXJpN.js"
+            // "startServerCommand": "npm run start",
+            // "startServerReadyPattern": "Local:",
+            // "url": [
+            //     "http://localhost:3000/",
+            // ],
+            // "numberOfRuns": 1
+        },
+        assert: {
+			"assertions": {
+				"accessibility": "warn",
+				"performance": "warn",
+				"seo": "warn",
+				"best-practices": "warn"
+			}
+		},
+        upload: {
+            "target": "filesystem",
+			"outputDir": "./.lighthouseci/reports"
+        },
+    },
+};
