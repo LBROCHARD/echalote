39b0995ae91358dbba0162f8edbaf8be88c45d33
LBROCHARD <lewis.brochard@gmail.com>
2025-08-21 12:40:39 +0200

    fea: logo on home page

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,12 +1,14 @@
 import { useAuth } from "@/providers/AuthContext";
+import logo from '/SuperNotes_icon.png';
 
 const HomeComponent = () => {
     const {user} = useAuth();
 
     return (
         <div className="flex flex-col flex-1 w-full items-center justify-center text-muted-foreground">
+            <img src={logo} alt="Logo Super Notes"/>
             <p>Hello <b>{user?.username}</b>, welcome to Super Notes !</p>
-            <p>You can create groups and pages with the left bar</p>
+            <p>You can create groups and pages using the left bar.</p>
         </div>
     );
 }
